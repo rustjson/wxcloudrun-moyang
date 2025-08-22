15f7d875f45e0f8d4c952ea7576d568538c96f51
Jason Yang <4018764+rustjson@users.noreply.github.com>
2025-08-22 23:28:34 +0800

    footer format

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,16 +1,14 @@
 function Footer() {
   return (
     <footer className="footer">
-      <div className="footer-content">
-        <p>&copy; 2025 Moyang. All rights reserved.</p>
-        <a
-          href="https://beian.mps.gov.cn/#/query/webSearch"
-          target="_blank"
-          className="footer-link"
-        >
-          滇ICP备2025067734号-1
-        </a>
-      </div>
+      <p>&copy; 2025 暮阳. All rights reserved.</p>
+      <a
+        href="https://beian.mps.gov.cn/#/query/webSearch"
+        target="_blank"
+        className="footer-link"
+      >
+        滇ICP备2025067734号-1
+      </a>
     </footer>
   );
 }
